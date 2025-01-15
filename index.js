254848c9e223836cf27e2e7f0a0a2bca43f49362
Javier Rosales <cyjav.dev@gmail.com>
2025-01-14 23:08:40 -0600

    Allow requests from all origins

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ const requestLogger = (request, response, next) => {
 }
 
 const app = express()
+const cors = require('cors')
+
+app.use(cors())
+
 app.use(express.json())
 app.use(requestLogger)
 
